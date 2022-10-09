88d3b7a846ca4bac6ab61ee41fa95d1f867acda0
sonhmai <>
2022-10-09 10:49:33 +0700

    Add impl using ArrayDeque

---
+++ src/main/scala/example/MinStackArrayDeque.scala
@@ -0,0 +1,33 @@
+package example
+
+class MinStackArrayDeque() extends MinStack {
+
+  private val stack = new java.util.ArrayDeque[Int]()
+  private val mins = new java.util.ArrayDeque[Int]()
+
+  // TODO - how to abstract out the methods below for Deque
+  //  This will be repetitive amongst MinStack impls using classes impl iava.util.Deque<E>
+  def push(value: Int): Unit = {
+    stack.push(value)
+    val nextMin: Int = Option(mins.peekFirst()) match {
+      // first element of both stacks
+      case Some(currentMin) if currentMin <= value => currentMin
+      case _ => value
+    }
+    mins.push(nextMin)
+  }
+
+  def pop(): Unit = {
+    stack.pop()
+    mins.pop()
+  }
+
+  def top(): Int = {
+    stack.peekFirst()
+  }
+
+  def getMin(): Int = {
+    mins.peekFirst()
+  }
+
+}
---
+++ src/test/scala/example/MinStackArrayDequeSpec.scala
@@ -0,0 +1,32 @@
+package example
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class MinStackArrayDequeSpec extends AnyFlatSpec with Matchers {
+  "stack" should "work in case 1 " in {
+    val stack = new MinStackArrayDeque()
+    stack.push(-2)
+    stack.push(0)
+    stack.push(-3)
+    stack.getMin() shouldBe -3
+    stack.pop()
+    stack.top() shouldBe 0
+    stack.getMin() shouldBe -2
+  }
+
+  "stack" should "work in case 2" in {
+    val stack = new MinStackArrayDeque()
+    stack.push(2)
+    stack.push(0)
+    stack.push(3)
+    stack.push(0)
+    stack.getMin() shouldBe 0
+    stack.pop()
+    stack.getMin() shouldBe 0
+    stack.pop()
+    stack.getMin() shouldBe 0
+    stack.pop()
+    stack.getMin() shouldBe 2
+  }
+}
